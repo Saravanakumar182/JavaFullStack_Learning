87255c9e0f3b279586e4433c1c51aa11dcb27b5e
Saravanakumar182 <saravanakumarsenbagavel@gmail.com>
2025-07-06 13:41:58 +0530

    week-3 updated

--- week-3_SpringDataJPAwithSpringBootAndHibernate/orm-project/orm-learn/src/main/java/com/cognizant/ormlearn/model/Country.java
+++ week-3_SpringDataJPAwithSpringBootAndHibernate/orm-project/orm-learn/src/main/java/com/cognizant/ormlearn/model/Country.java
@@ -1,19 +1,18 @@
 package com.cognizant.ormlearn.model;
 
-import org.springframework.data.annotation.Id;
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
+
 @Table(name = "country")
+@Entity
 public class Country {
 
     @Id
-    @Column(name = "code")
     private String code;
 
-    @Column(name = "name")
     private String name;
 
     public String getCode() {
