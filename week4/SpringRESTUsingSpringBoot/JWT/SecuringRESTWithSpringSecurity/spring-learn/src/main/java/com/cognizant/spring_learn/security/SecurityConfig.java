13d15cb6b2c55aac830efa19e859aea6a76a9c96
Saravanakumar182 <saravanakumarsenbagavel@gmail.com>
2025-07-12 23:03:33 +0530

    week-4 output screenshots and jwt hands on

---
+++ week4/SpringRESTUsingSpringBoot/JWT/SecuringRESTWithSpringSecurity/spring-learn/src/main/java/com/cognizant/spring_learn/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.cognizant.spring_learn.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+}
